d7aed350c62cde1e3575f475818673890344475e
Jeran <61069933+JeranJB@users.noreply.github.com>
2020-07-22 13:09:10 -0600

    First Commit

---
+++ readme.txt
@@ -0,0 +1,12 @@
+1. Define your project
+	The goal of this project is to do a simple log-in page that uses HTML, CSS, JS, PHP, and SQL.
+	The audience will probably be just me, or maybe an employer if I really like it.
+	
+2. Plan out everything
+	I plan to have 3 html pages, One with a simple login page, and the other with the JS lightshow I saw on StudioWeb, and a 	register for an account page, which will look like the login page.
+	I want to be able to register an account, store that on a SQL database, then be able to log in.
+	It will have a blog in the center of the index page, with LOG IN and REGISTER and two inputs for Username and Password 	
+	The only navigation will be log in, register, and log out.
+	
+3. Sketch my Ideas
+	I will create these 3 html pages in paint.
---
+++ scripts/lightshow.js
@@ -0,0 +1,49 @@
+var notRunning = true; // prevents double-run bug.
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF".split("");
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function discoSquare() {
+  var canvas = document.getElementById("whiteBoard");
+  var context = canvas.getContext("2d");
+
+  var verPos = Math.floor(Math.random() * 399 + 1);
+  var theColor = getRandomColor();
+
+  context.lineWidth = 10;
+  context.strokeStyle = theColor;
+  context.moveTo(10, verPos); // (horizontal / vertical)
+  console.log("vertical pos: " + verPos);
+
+  context.lineTo(400, verPos);
+  context.lineCap = "butt";
+  context.stroke();
+
+  document.getElementById("displayColor").innerHTML = theColor;
+}
+
+var animation;
+
+document.getElementById("start").onclick = function () {
+  if (notRunning) {
+    animation = setInterval(discoSquare, 100);
+    notRunning = false;
+  } else {
+    alert("Sorry, already running.");
+  }
+};
+
+document.getElementById("stop").onclick = function () {
+  clearInterval(animation);
+  notRunning = true;
+};
+
+document.getElementById("logout").onclick = function () {
+  window.location.href = "index.html";
+};
